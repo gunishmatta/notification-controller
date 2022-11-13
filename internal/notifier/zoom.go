190b40b16171bad928e3a7da3c4a2fca562eeaeb
gunishmatta <gunishmatta@gmail.com>
2022-11-13 14:09:53 +0000

    added zoom payload

--- internal/notifier/zoom.go
+++ internal/notifier/zoom.go
@@ -1,4 +1,17 @@
-type Zoom struct {
+package notifier
 
-	
+type Zoom struct {
+	URL   string
+	Token string
+}
+
+type ZoomPayload struct {
+	RobotJid  string `json:"robot_jid"`
+	ToJid     string `json:"to_jid"`
+	AccountId string `json:"account_id"`
+	Content  ZoomContent `json:"content"`
+}
+
+type ZoomContent struct {
+
 }
